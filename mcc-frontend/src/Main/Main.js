7cac1a6784060823504ea8412d8a2e7549e183b2
SEROs0 <ttkk0062@gmail.com>
2026-05-22 22:01:12 +0700

    Bulid Project-Frontend

---
+++ mcc-frontend/src/Main/Main.js
@@ -0,0 +1,37 @@
+import './Main.css';
+
+function MainPage() {
+
+    
+
+    return (
+        <div className='boxmain'>
+            <div>
+                <h1 style={{color: 'white', backgroundColor: 'rgb(65, 65, 65)', width:'50vh', height:'10vh' }}>MedCare Clinic</h1>
+                    <div  className='sidebar'>
+                        <div className='sidebar'>
+                            <button>จองคิว</button>
+                       
+                            <button>Dashboard</button>
+                       
+                            <button>ประวัติผู้ป่วย</button>
+                        
+                            <button>แจ้งเตือน</button>
+                        
+                            <button>User</button>
+                        </div>
+                    </div>
+            </div>
+            <div>
+                <div>
+                    <h1>sdfgsdfg</h1>
+                </div>
+            </div>
+        </div>
+
+
+
+    );
+}
+export default MainPage;
+
